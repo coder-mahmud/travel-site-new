ee9971cab3f677fa8b3e64bd7c03a4c0579a533b
Mahmud <mahmud.linked@gmail.com>
2018-09-30 15:03:25 +0600

    After escaping css error on gulp and keep gulp running

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,8 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var cssimport = require('postcss-import');
-var browserSync = require('browser-sync').create();
-
-gulp.task('default', function(){
-	console.log("Hello world!")
-});
-
-gulp.task('html', function(){
-	console.log("Imagine this is actual task");
-});
-
-gulp.task('styles', function(){
-	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssimport, cssvars,nested, autoprefixer]))
-	.pipe(gulp.dest('./app/temp/styles'));
-});
-
-gulp.task('watch', function(){
-
-	browserSync.init({
-		notify:false,
-		server:{
-			baseDir: 'app' 
-		}
-	});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
 
-	watch('./app/index.html', function(){
-		browserSync.reload();
-	});
-
-	watch('./app/assets/styles/**/*.css', function(){
-		//gulp.start('styles');
-		gulp.start('cssInject');
-	});
 
 
-});
 
 
-gulp.task('cssInject', ['styles'], function(){
-	return gulp.src('./app/temp/styles/styles.css')
-	.pipe(browserSync.stream());
-});
